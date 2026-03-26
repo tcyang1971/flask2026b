cd5493c14d893f7e76238b543f7515e92293b38f
楊子青 (Tzyy-Ching Yang) <tcyang@gm.pu.edu.tw>
2026-03-26 11:20:14 +0800

    vercel_v0

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
 
 app = Flask(__name__)
@@ -9,11 +9,13 @@ def index():
     link += "<a href=/mis>課程</a><hr>"
     link += "<a href=/today>現在日期時間</a><hr>"
     link += "<a href=/me>關於我</a><hr>"
+    link += "<a href=/welcome?u=子青&d=靜宜資管&c=資訊管理導論>Get傳值</a><hr>"
+    link += "<a href=/account>POST傳值</a><hr>"
     return link
 
 @app.route("/mis")
 def course():
-    return "<h1>資訊管理導論</h1>"
+    return "<h1>資訊管理導論</h1><a href=/>返回首頁</a>"
 
 @app.route("/today")
 def today():
@@ -23,6 +25,25 @@ def today():
 @app.route("/me")
 def me():
     return render_template("mis2026b.html")
+
+@app.route("/welcome", methods=["GET"])
+def welcome():
+    user = request.values.get("u")
+    d = request.values.get("d")
+    c = request.values.get("c")    
+    return render_template("welcome.html", name= user, dep = d, course = c)
+
+
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    if request.method == "POST":
+        user = request.form["user"]
+        pwd = request.form["pwd"]
+        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
+        return result
+    else:
+        return render_template("account.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
